9108e7918e8e8f3a82b996342af85f8354f96d89
xuthus <xuthus5@gmail.com>
2020-01-09 14:35:51 +0800

    release:Integration project

--- Togo/models/subject.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Subject struct {
-	Id  int64 //主键
-	Uid int64 //用户ID
-	Iid int64 //课题ID
-}
--- Togo/utils/conv_time.go
+++
@@ -1,8 +0,0 @@
-package utils
-
-import "time"
-
-//返回当前日期时间
-func GetNowDateTime() string {
-	return time.Now().Format("2006-01-02 15:04:05")
-}
